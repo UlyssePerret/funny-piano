1d3540a899de2e0a67d95a37351ec9887a77c6a4
Perret Ulysse <ulysseperret@orange.fr>
2020-10-21 13:45:05 +0200

    test change style3

--- src/Components/VHeader.vue
+++ src/Components/VHeader.vue
@@ -1,17 +1,35 @@
 <template>
 <header>
   <div class="bloc-titre">
-        <h1 class="text-texte">The emoji piano</h1>
+        <h1 class="text-texte text-couleur">The emojia piano 2</h1>
     </div>
       <div  >
-        <h1 class="text-couleu">balaba</h1>
+        <p class="text-couleur">balaba</p>
     </div>
 </header>
   
       
 </template>
 <style lang="postcss" scoped>
-
-
+    .text-couleur{
+        color: red;  
+    }
+        .text-texte{
+            /*issue figmae*/
+            position: absolute;
+            left: 9.34%;
+            right: 9.34%;
+            top: 21.67%;
+            bottom: 21.67%;
+            
+            font-family: "PT Mono";
+            font-style: normal;
+            font-weight: normal;
+            font-size: 30px;
+            line-height: 34px;
+            /* identical to box height */
+            text-align: center;
+            /*color: #FFFFFF;   atester aprés test couleur*/
+        }
 
 </style>

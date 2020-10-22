c4e8cf0beb3698d7b7dc343ab0ebefc6d71375fa
Emmanuel Durgoni <emmanuel@upian.com>
2020-10-22 11:17:30 +0200

    fix architecture

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
+import './assets/js/piano'
 import './assets/tailwind.css'
 import './assets/main.css'
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+console.log('hello')
